5a05df5324b07b7d3f6594c91e9e7b9227ec3525
roksana62 <aminationcraft62@gmail.com>
2024-04-06 23:19:17 +0600

    second calculator done

---
+++ app.js
@@ -0,0 +1,24 @@
+let outputScreen= document.getElementById('output_screen');
+
+function display(num){
+    outputScreen.value+= num;
+}
+
+function calculate(){
+    try{
+        outputScreen.value= eval(outputScreen.value);
+
+    }
+    catch(err){
+        alert('Invalid');
+   
+    }
+}
+
+function clearly(){
+  outputScreen.value= '';
+}
+
+function del (){
+    outputScreen.value= outputScreen.value.slice(0, -1)
+}
